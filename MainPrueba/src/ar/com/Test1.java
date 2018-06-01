fd26b993833638ccd2406c55c788cdd98f5a275b
famrodriguez <famrodriguez@INDRA411.tasa.telefonica.com.ar>
2018-06-01 18:04:18 -0300

    Mas log-

--- MainPrueba/src/ar/com/Test1.java
+++ MainPrueba/src/ar/com/Test1.java
@@ -3,8 +3,11 @@ package ar.com;
 public class Test1 {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Cambio 1ro de Junio !! es Viernes");
-
+		
+		System.out.println("nuevo log");
+		
 	}
 
 }
